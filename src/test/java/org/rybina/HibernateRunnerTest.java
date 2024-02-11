8608b9e172ced5cf57934e043200e147202f1e6d
rybinali <rybinaaaaa.a@gmail.com>
2024-02-11 18:01:34 +0100

    add realization of template query by using java reflection

---
+++ src/test/java/org/rybina/HibernateRunnerTest.java
@@ -0,0 +1,57 @@
+package org.rybina;
+
+import org.junit.jupiter.api.Test;
+import org.rybina.entity.User;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Optional.*;
+import static java.util.stream.Collectors.*;
+
+public class HibernateRunnerTest {
+
+    @Test
+    void checkReflectionApi() throws SQLException, IllegalAccessException {
+        User user = User.builder()
+                .lastName("lastN2")
+                .age(20)
+                .firstname("firstn")
+                .username("test3")
+                .birthday(LocalDate.of(2004, 2, 28))
+                .build();
+
+        String sql = """
+                insert into %s (%s) values (%s)
+                """;
+        String tableName = ofNullable(user.getClass().getAnnotation(Table.class))
+                .map(table -> table.schema() + "." + table.name()).orElse(user.getClass().getName());
+
+        Field[] declaredFields = user.getClass().getDeclaredFields();
+        String columnNames = Arrays.stream(declaredFields).map(field -> ofNullable(field.getAnnotation(Column.class)).map(Column::name).orElse(field.getName())).collect(joining(","));
+
+        String columnValues = Arrays.stream(declaredFields).map(field -> "?").collect(joining(","));
+
+        String readySqlQuery = sql.formatted(tableName, columnNames, columnValues);
+
+        System.out.println(readySqlQuery);
+
+        Connection connection = null;
+
+        PreparedStatement preparedStatement = connection.prepareStatement(readySqlQuery);
+        int index = 0;
+        for (Field declaredField : declaredFields) {
+            index++;
+            declaredField.setAccessible(true);
+            preparedStatement.setObject(index, declaredField.get(user));
+        }
+    }
+}
