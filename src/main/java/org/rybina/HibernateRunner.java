49262e28667488040ded46c2b880952ab52a9cce
rybinali <rybinaaaaa.a@gmail.com>
2024-02-12 13:25:58 +0100

    add basic usage of logging

--- src/main/java/org/rybina/HibernateRunner.java
+++ src/main/java/org/rybina/HibernateRunner.java
@@ -2,16 +2,21 @@ package org.rybina;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.rybina.entity.Birthday;
 import org.rybina.entity.User;
 import org.rybina.util.HibernateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class HibernateRunner {
 
-    public static void main(String[] args) throws SQLException {
+    public static final Logger logger = LoggerFactory.getLogger(HibernateRunner.class);
+
+    public static void main(String[] args) {
+
         User user = User.builder()
                 .lastName("Alina")
                 .firstname("Rybina")
@@ -19,29 +24,22 @@ public class HibernateRunner {
                 .birthday(new Birthday(LocalDate.of(2004, 4, 12)))
                 .build();
 
+        logger.info("User entity is in transient state, object {}", user);
+
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
-
-                session1.beginTransaction();
-
-                session1.getTransaction().commit();
-
-                session1.beginTransaction();
+                Transaction transaction = session1.beginTransaction();
+                logger.trace("Transaction is created {}", transaction);
 
                 session1.saveOrUpdate(user);
-                user.setFirstname("kkk");
 
-                System.out.println(session1.isDirty());
-
+                logger.trace("User is in persistence state {}, session {}", user, session1);
                 session1.getTransaction().commit();
-            }
 
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-//                session2.delete(user);
-
-                session2.getTransaction().commit();
+                logger.warn("User is in detached state {}, session is closed {}", user, session1);
+            } catch (Exception exception) {
+                logger.error("Exception occured", exception);
+                throw exception;
             }
         }
     }
