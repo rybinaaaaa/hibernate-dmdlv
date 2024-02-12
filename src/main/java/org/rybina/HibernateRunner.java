ac223406b609fb4ab43fcd7d4aca151b0971d1a8
rybinali <rybinaaaaa.a@gmail.com>
2024-02-12 13:40:52 +0100

    creating own appender

--- src/main/java/org/rybina/HibernateRunner.java
+++ src/main/java/org/rybina/HibernateRunner.java
@@ -1,5 +1,6 @@
 package org.rybina;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,9 +12,8 @@ import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
+@Slf4j
 public class HibernateRunner {
-
-    public static final Logger logger = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) {
 
@@ -24,21 +24,21 @@ public class HibernateRunner {
                 .birthday(new Birthday(LocalDate.of(2004, 4, 12)))
                 .build();
 
-        logger.info("User entity is in transient state, object {}", user);
+        log.info("User entity is in transient state, object {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
                 Transaction transaction = session1.beginTransaction();
-                logger.trace("Transaction is created {}", transaction);
+                log.trace("Transaction is created {}", transaction);
 
                 session1.saveOrUpdate(user);
 
-                logger.trace("User is in persistence state {}, session {}", user, session1);
+                log.trace("User is in persistence state {}, session {}", user, session1);
                 session1.getTransaction().commit();
 
-                logger.warn("User is in detached state {}, session is closed {}", user, session1);
+                log.warn("User is in detached state {}, session is closed {}", user, session1);
             } catch (Exception exception) {
-                logger.error("Exception occured", exception);
+                log.error("Exception occured", exception);
                 throw exception;
             }
         }
